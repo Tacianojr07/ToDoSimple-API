d559cc73d8f998d56ebc0e872de60804c06c3032
Tacianojr07 <t.juniior13@gmail.com>
2023-08-10 21:04:11 -0300

    adding validation

--- src/main/java/com/tacianojr07/todosimple/models/User.java
+++ src/main/java/com/tacianojr07/todosimple/models/User.java
@@ -14,6 +14,10 @@ import javax.validation.constraints.Size;
 @Entity
 @Table (name = "User.TABLE_NAME") 
 public class User {
+
+    public interface CreateUser{}
+    public interface UpdateUser{}
+
     public static final String TABLE_NAME = "user";
 
     @Id
@@ -22,15 +26,15 @@ public class User {
     private Long id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
-    @NotNull
-    @NotEmpty
-    @Size(min = 2, max = 100 )
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100 )
     private String username;
 
     @Column(name = "password", length = 60, nullable = false)
-    @NotNull
-    @NotEmpty
-    @Size(min = 8, max = 60 )
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60 )
     private String password;
 
 }
