53f49a8997055605aefe315c1adcf0789c50febd
Tacianojr07 <t.juniior13@gmail.com>
2023-08-12 15:57:02 -0300

    creating getters, setters and constructors

--- src/main/java/com/tacianojr07/todosimple/models/User.java
+++ src/main/java/com/tacianojr07/todosimple/models/User.java
@@ -37,4 +37,43 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60 )
     private String password;
 
+    //private List<Task> tasks = new ArrayList<Task>
+
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 }
