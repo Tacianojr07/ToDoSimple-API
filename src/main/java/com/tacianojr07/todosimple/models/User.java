73b7194cf428bd0805bf1befb225d83641469cb3
Tacianojr07 <t.juniior13@gmail.com>
2023-08-12 16:09:59 -0300

    equals and hashcode

--- src/main/java/com/tacianojr07/todosimple/models/User.java
+++ src/main/java/com/tacianojr07/todosimple/models/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Objects;
 
 
 @Entity
@@ -74,6 +75,29 @@ public class User {
         this.password = password;
     }
 
+    @Override
+    public boolean equals (Object obj) {
+        if( obj == this) {
+            return true;
+        }
+        if (!obj instanceof User) {
+            return false;
+        }
+        if(obj == null) { 
+            return false;
+        }
+
+        User other = (User) obj;
+
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+        return result;
+    }
 
 
 }
