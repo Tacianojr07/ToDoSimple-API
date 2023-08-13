33d81bb85bdab9aebfdf539411c2aa247b8c6b72
Tacianojr07 <t.juniior13@gmail.com>
2023-08-13 16:50:09 -0300

    creating TaskService for Task

---
+++ src/main/java/com/tacianojr07/todosimple/services/TaskService.java
@@ -0,0 +1,50 @@
+package com.tacianojr07.todosimple.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tacianojr07.todosimple.models.Task;
+import com.tacianojr07.todosimple.models.User;
+import com.tacianojr07.todosimple.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserServices userServices;
+
+    public Task findById(Long id){
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException("Tarefa não encontrada! " + id + "  Tipo: " + Task.class.getName()));
+    }
+
+    @Transactional
+    public Task create(Task obj) {
+        User user = this.userServices.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        return obj;
+    }
+
+    public Task update(Task obj) {
+        Task newObj = findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
+
+    public void delete(long id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível deletar");
+        }
+    }
+}
