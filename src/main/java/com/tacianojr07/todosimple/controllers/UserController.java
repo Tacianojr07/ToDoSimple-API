beab1040973832b58836dbfcbe89a2acf5338ae4
Tacianojr07 <t.juniior13@gmail.com>
2023-08-16 11:24:49 -0300

    edit

--- src/main/java/com/tacianojr07/todosimple/controllers/UserController.java
+++ src/main/java/com/tacianojr07/todosimple/controllers/UserController.java
@@ -15,13 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tacianojr07.todosimple.models.User;
 import com.tacianojr07.todosimple.models.User.CreateUser;
-import com.tacianojr07.todosimple.models.User.UpdateUser;
 import com.tacianojr07.todosimple.services.UserServices;
 
 @RestController
