db19367fe776977adfbb660d1c7b75c76d7d96c2
Tacianojr07 <t.juniior13@gmail.com>
2023-08-21 16:08:13 -0300

    create class for errors in the app

---
+++ src/main/java/com/tacianojr07/todosimple/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,121 @@
+package com.tacianojr07.todosimple.Exceptions;
+
+
+
+
+
+
+import javax.validation.ConstraintViolationException;
+
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j(topic= "GLOBAL_EXCEPTION_HANDLER")
+@RestControllerAdvice
+public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler{
+    
+    @Value("${server.error.include-exception}")
+    private boolean printStackTrace;
+
+    //modificando o padrão do spring sobre erros
+    @Override
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException methodArgumentNotValidException,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error. Check 'errors' field for details.");
+        for (FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()) {
+            errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.unprocessableEntity().body(errorResponse);
+    }
+
+    //captura  erro de forma geral caso o erro que dê não esteja tratado
+    public ResponseEntity<Object> handleAllUncaughtException(
+        Exception exception,
+        WebRequest request
+    ) {
+        final String errorMensage = "Unknown error occured";
+        log.error(errorMensage, exception);
+        return buildErrorResponse(
+            exception,
+            errorMensage,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            request
+        );
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(
+        Exception exception,
+        String message,
+        HttpStatus httpStatus,
+        WebRequest request
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+        if (this.printStackTrace) {
+            errorResponse.setStackTracer(ExceptionUtils.getStackTrace(exception));
+        }
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+    //captura erro que vier do usuario ex: tentar realizar um post com um username que já existe no banco de dados
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<Object> handleDataIntegrityViolantionException(
+        DataIntegrityViolationException dataIntegrityViolationException,
+        WebRequest request
+    ) {
+        String errorMessage = dataIntegrityViolationException.getMostSpecificCause().getMessage();
+        log.error("Failed to save entity with integrity problems: " + errorMessage, dataIntegrityViolationException);
+
+        return buildErrorResponse(
+            dataIntegrityViolationException,
+            errorMessage,
+            HttpStatus.CONFLICT,
+            request
+        );
+    }
+
+    //impede um senha seja enviado de forma vazia
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<Object> handleConstraintViolantionException(
+        ConstraintViolationException constraintViolationException,
+        WebRequest request
+    ) {
+        log.error("Falied to validate element", constraintViolationException);
+        return buildErrorResponse(
+            constraintViolationException,
+            HttpStatus.UNPROCESSABLE_ENTITY,
+            request
+        );
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(
+        Exception exception,
+        HttpStatus httpStatus,
+        WebRequest request) {
+            return buildErrorResponse(exception, exception.getMessage(), httpStatus, request);
+        }
+    
+    
+}
